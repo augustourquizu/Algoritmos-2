663a3a7c525c3a4afdaf130ab62c11009a538118
AugustoUrquizu <augusto.urquizu@gmail.com>
2025-03-29 00:33:54 -0300

    m

---
+++ taller 2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
@@ -0,0 +1,44 @@
+package aed;
+
+class ArregloRedimensionableDeRecordatorios {
+
+//     Ejercicio 11. Implementar los m´etodos longitud, agregarAtras y obtener de la clase
+// ArregloRedimensionableDeRecordatorios.
+// El m´etodo longitud devuelve la cantidad de elementos en la secuencia.
+// El m´etodo agregarAtras, extiende la secuencia con el elemento recibido por par´ametro.
+// El m´etodo obtener devuelve el elemnto en la posici´on i.
+    public ArregloRedimensionableDeRecordatorios() {
+        // Implementar
+    }
+
+    public int longitud() {
+
+       return 0 ;
+    }
+
+    public void agregarAtras(Recordatorio i) {
+        // Implementar
+    }
+
+    public Recordatorio obtener(int i) {
+        // Implementar
+        return null;
+    }
+
+    public void quitarAtras() {
+        // Implementar
+    }
+
+    public void modificarPosicion(int indice, Recordatorio valor) {
+        // Implementar
+    }
+
+    public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
+        // Implementar
+    }
+
+    public ArregloRedimensionableDeRecordatorios copiar() {
+        // Implementar
+        return null;
+    }
+}
